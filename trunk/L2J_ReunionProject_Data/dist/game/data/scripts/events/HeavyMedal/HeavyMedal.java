8ac582b22f95558b6ef04b6cf78b1e0ce0a40daf
vGodFather <vGodFather@31e9427f-97bb-4a39-bcb8-0e340e989537>
2013-12-22 22:45:42 +0000

    Before Apply new Updates Create a backup first don't tell me that i didn't warn you!
    Update scripts.ini located in game/data/
    Delete all chars folder and paste from this rev located in game/data/stats/
    Delete all scripts folder and paste from this rev located in game/data/
    
    And Important if you have already running server execute sql files:
    1.Updates 23-12-2013.sql
    Located in sql/game/updates/
    
    YOU MUST FOLLOW EVERY STEP!!! wait for core commit

--- trunk/L2J_ReunionProject_Data/dist/game/data/scripts/events/HeavyMedal/HeavyMedal.java
+++ trunk/L2J_ReunionProject_Data/dist/game/data/scripts/events/HeavyMedal/HeavyMedal.java
@@ -18,6 +18,7 @@
  */
 package events.HeavyMedal;
 
+import l2r.gameserver.enums.QuestSound;
 import l2r.gameserver.model.actor.L2Npc;
 import l2r.gameserver.model.actor.instance.L2PcInstance;
 import l2r.gameserver.model.event.LongTimeEvent;
