8823491c311ca04a6ca7a3a0fcac2f1c7197e77b
vGodFather <vGodFather@31e9427f-97bb-4a39-bcb8-0e340e989537>
2014-08-02 19:43:54 +0000

    Fixed DamOverTime and DamOverTimePercent skills now they work fine Clean up Stun effect

--- trunk/L2J_ReunionProject_Data/dist/game/data/scripts/handlers/effecthandlers/DamOverTimePercent.java
+++ trunk/L2J_ReunionProject_Data/dist/game/data/scripts/handlers/effecthandlers/DamOverTimePercent.java
@@ -33,13 +33,13 @@ public class DamOverTimePercent extends L2Effect
 	{
 		super(env, template);
 	}
-
+	
 	@Override
 	public L2EffectType getEffectType()
 	{
 		return L2EffectType.DMG_OVER_TIME_PERCENT;
 	}
-
+	
 	@Override
 	public boolean onActionTime()
 	{
@@ -47,9 +47,9 @@ public class DamOverTimePercent extends L2Effect
 		{
 			return false;
 		}
-
+		
 		double damage = getEffected().getCurrentHp() * calc();
-
+		
 		if (damage >= (getEffected().getCurrentHp() - 1))
 		{
 			if (getSkill().isToggle())
@@ -57,21 +57,21 @@ public class DamOverTimePercent extends L2Effect
 				getEffected().sendPacket(SystemMessageId.SKILL_REMOVED_DUE_LACK_HP);
 				return false;
 			}
-
+			
 			// For DOT skills that will not kill effected player.
 			if (!getSkill().killByDOT())
 			{
 				// Fix for players dying by DOTs if HP < 1 since reduceCurrentHP method will kill them
 				if (getEffected().getCurrentHp() <= 1)
 				{
-					return getSkill().isToggle();
+					return true;
 				}
-
+				
 				damage = getEffected().getCurrentHp() - 1;
 			}
 		}
 		getEffected().reduceCurrentHpByDOT(damage, getEffector(), getSkill());
-
-		return getSkill().isToggle();
+		
+		return true;
 	}
 }
