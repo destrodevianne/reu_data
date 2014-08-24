6f69a0902c025d6212d9df644af18ae6876b26aa
vGodFather <vGodFather@31e9427f-97bb-4a39-bcb8-0e340e989537>
2014-08-24 11:47:19 +0000

    More java quests
    Delete scripts folder and paste from this rev
    
    For lvie servers execute sql file
    Updates 24-08-2014.sql tools/sql/game/updates/

---
+++ trunk/L2J_ReunionProject_Data/dist/tools/sql/game/updates/Updates 24-08-2014.sql
@@ -0,0 +1,9 @@
+UPDATE character_quests SET name='406_PathToElvenKnight' WHERE name='Q00406_PathOfTheElvenKnight';
+UPDATE character_quests SET name='408_PathToElvenwizard' WHERE name='Q00408_PathOfTheElvenWizard';
+UPDATE character_quests SET name='409_PathToOracle' WHERE name='Q00409_PathOfTheElvenOracle';
+UPDATE character_quests SET name='410_PathToPalusKnight' WHERE name='Q00410_PathOfThePalusKnight';
+UPDATE character_quests SET name='411_PathToAssassin' WHERE name='Q00411_PathOfTheAssassin';
+UPDATE character_quests SET name='412_PathToDarkwizard' WHERE name='Q00412_PathOfTheDarkWizard';
+UPDATE character_quests SET name='413_PathToShillienOracle' WHERE name='Q00413_PathOfTheShillienOracle';
+UPDATE character_quests SET name='632_NecromancersRequest' WHERE name='Q00632_NecromancersRequest';
+UPDATE character_quests SET name='660_AidingtheFloranVillage' WHERE name='Q00660_AidingTheFloranVillage';
